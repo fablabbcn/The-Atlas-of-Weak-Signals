aa2240a16651c63f8196f7f78baf4a4d5e0f6620
Lucaslpena <lucaslpena@gmail.com>
2019-10-02 13:58:09 +0200

    basic draft copy done

--- src/js/script.js
+++ src/js/script.js
@@ -1,30 +1,31 @@
-// var text1 = new Blotter.Text("under construction 👁‍🗨", {
-//     family : "Overpass Mono",
-//     size : 84,
-//     fill : "#171717"
-// });
-//
-// var text2 = new Blotter.Text("Sup", {
-//     family : "Overpass Mono",
-//     size : 120,
-//     fill : "#171717"
-// });
-//
-// var material = new Blotter.ChannelSplitMaterial();
-//
+var text1 = new Blotter.Text("under construction 👁‍🗨", {
+    family : "Overpass Mono",
+    size : 84,
+    fill : "#171717"
+});
+
+var text2 = new Blotter.Text("ATLAS OF WEAK SIGNALS", {
+    family : "'Nexa Bold', sans-serif",
+    size : 54,
+    fill : "#171717"
+});
+
+var material = new Blotter.ChannelSplitMaterial();
+var material2 = new Blotter.FliesMaterial();
+
 // var blotter1 = new Blotter(material, { texts : text1 });
 // var scope1 = blotter1.forText(text1);
 // scope1.appendTo(document.body);
-//
-// // var blotter2 = new Blotter(material, { texts : text2 });
-// // var scope2 = blotter2.forText(text2);
-// // scope2.appendTo(document.body);
-//
-// setTimeout(function(){
-//     var element = $('.b-canvas').detach();
-//     // $('.s00').append(element);
-// },2000);
-//
-// //https://codepen.io/SimonEvans/pen/PQKgyQ
-// // https://github.com/bradley/blotter
-// // https://blotter.js.org/#/documentation
+
+var blotter2 = new Blotter(material2, { texts : text2 });
+var scope2 = blotter2.forText(text2);
+scope2.appendTo(document.body);
+
+setTimeout(function(){
+    var element = $('.b-canvas').detach();
+    $('.modal').prepend(element);
+},2000);
+
+//https://codepen.io/SimonEvans/pen/PQKgyQ
+// https://github.com/bradley/blotter
+// https://blotter.js.org/#/documentation
